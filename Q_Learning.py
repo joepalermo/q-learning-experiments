5763959a98f62c196e4ea5566a745b19c514a4ce
Joseph Palermo <josephpalermo@Josephs-MacBook-Air.local>
2016-10-12 21:50:20 -0400

    reorganize Q-Learning main method

--- Q_Learning.py
+++ Q_Learning.py
@@ -1,8 +1,9 @@
 from Environment import Environment
 from Q_Table import Q_Table
+#from Simulation import run_comparative_simulation
 
 # define parameters
-gamma = 1
+gamma = 0.9
 num_training_episodes = 10000
 
 def run_episode(state, q_table, env):
@@ -18,14 +19,22 @@ def run_episode(state, q_table, env):
         state = next_state
     return episode_data
 
-def train_on_episode_data(episode_data, q_table):
+def train_on_episode(episode_data, q_table):
     for _ in range(len(episode_data)):
         step_data = episode_data.pop()
         q_table.update(step_data, gamma)
 
+# training_log is a list of training episodes
+def determine_performance_improvement(training_log):
+    interval_1 = training_log[0:10]
+    interval_2 = training_log[len(training_log)-10:len(training_log)]
+    print sum(interval_1)/float(len(interval_1))
+    print sum(interval_2)/float(len(interval_2))
+
+
 def main():
     # initializations
-    #training_log = []
+    training_log = []
     env = Environment()
     q_table = Q_Table(env)
 
@@ -36,15 +45,13 @@ def main():
         # run through an episode to completion, returning the sequence of state,
         # action and reward values
         episode_data = run_episode(initial_state, q_table, env)
-        #training_log.append(len(episode_data))
+        training_log.append(len(episode_data))
         # update the q_table on the data from the most recent episode
-        train_on_episode_data(episode_data, q_table)
-    #print q_table
-    return q_table.q_table #, training_log
+        train_on_episode(episode_data, q_table)
 
+    # assess training results
+    determine_performance_improvement(training_log)
+    # todo: run comparative simulations
+    #run_comparative_simulations(training_log[0], training_log[-1])
 
-#q_table, training_log = main()
-#interval_1 = training_log[0:100]
-#interval_2 = training_log[len(training_log)-100:len(training_log)]
-#print sum(interval_1)/float(len(interval_1))
-#print sum(interval_2)/float(len(interval_2))
+main()
