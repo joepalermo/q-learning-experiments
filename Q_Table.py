478b9762f8377ea6ee3c50fd6b992839078f7e0f
Joseph Palermo <josephwpalermo@gmail.com>
2017-03-25 18:34:43 -0400

    light cleanup to Q_Table.py

--- Q_Table.py
+++ Q_Table.py
@@ -6,10 +6,11 @@ class Q_Table:
         self.action_space = env.action_space
         self.q_table = env.initialize_q_table()
 
-    # get the best action from a given state according to current Q-Table
+    # get the best action from a given state according to current Q-table
     def get_best_action(self, state):
-        # get a shuffled version of self.action_space so that actions are chosen
-        # in random order, preventing an agent from getting stuck on an edge
+        # get a shuffled version of self.action_space so that actions are
+        # considered in random order, preventing an agent from getting stuck on
+        # an edge of the environment
         action_space = list(self.action_space)
         shuffle(action_space)
         action = action_space[0]
@@ -21,9 +22,9 @@ class Q_Table:
                 best_action = (action, self.q_table[(state, action)])
         return best_action[0]
 
+    # update the Q-table on the basis of step data
     def update(self, step_data, gamma):
         (state, action, reward, next_state) = step_data
         best_next_action = self.get_best_action(next_state)
         self.q_table[(state, action)] = \
-        reward + \
-        gamma * self.q_table[(next_state, best_next_action)]
+        reward + gamma * self.q_table[(next_state, best_next_action)]
