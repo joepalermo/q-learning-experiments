d0aaee3588e94fb7c592e45b53bd3994e5123528
Joseph Palermo <josephwpalermo@gmail.com>
2017-03-26 15:12:29 -0400

    refactor agent animation code

--- Agent_Animation.py
+++ Agent_Animation.py
@@ -9,17 +9,8 @@ http://programarcadegames.com/index.php?chapter=array_backed_grids
 import pygame
 import time
 
-# set HEIGHT and WIDTH of the screen
-WINDOW_SIZE = [255, 255]
-
 # set title of screen
-TITLE = "A Q-Learning Agent Tries to Find its Way Home..."
-
-# define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+TITLE = "A Q-Learning Agent Seeks Home..."
 
 # this sets the WIDTH and HEIGHT of each grid location
 WIDTH = 40
@@ -28,108 +19,111 @@ HEIGHT = 40
 # this sets the margin between each cell
 MARGIN = 5
 
-# utility method to re-generate a black grid before each frame is drawn
-def generate_black_grid(num_rows, num_cols):
-    grid = []
-    for row in range(num_rows):
-        grid.append([])
-        for column in range(num_cols):
-            grid[row].append(0)
-    return grid
+# define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
+# set the update interval to 200 ms
+UPDATE_INTERVAL = 200
 
+# run an animation of an episode
 def run_animation(num_cols, num_rows, episode_data):
-    # some initializations
+    # some pygame initializations
     pygame.init()
     pygame.display.set_caption(TITLE)
-    screen = pygame.display.set_mode(WINDOW_SIZE)
+    window_width = WIDTH * num_rows + MARGIN * (num_rows + 1)
+    window_height = HEIGHT * num_cols + MARGIN * (num_cols + 1)
+    window_size = [window_width, window_height]
+    screen = pygame.display.set_mode(window_size)
 
-    # loop until the user clicks the close button.
-    done = False
-
-    # used to manage how fast the screen updates
+    # for keeping track of time, to determine when to update the screen
     clock = pygame.time.Clock()
+    time_since_update = 0
 
-    # time elapsed since last action was shown
-    time_elapsed_since_action_update = 0
+    # get a list of states in the episode
+    episode_states = get_episode_states(episode_data)
+    episode_length = len(episode_states)
     state_index = 0
 
-    # length of the episode
-    episode_length = len(episode_data)
-
-    # main loop
+    # animation loop
+    done = False
     while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # If user clicked close
-                done = True  # Flag that we are done so we exit this loop
 
         # dt <- ms since clock was initialized or since last tick()
         dt = clock.tick()
-        time_elapsed_since_action_update += dt
+        time_since_update += dt
 
-        # if more than 200 ms have elapsed since last action update, then update
-        if time_elapsed_since_action_update > 200 and \
-           state_index < episode_length:
+        # if more than 200 ms have elapsed since last update, and if there is
+        # more step data in the episode, then perform an update
+        if time_since_update > UPDATE_INTERVAL and state_index < episode_length:
 
             # get state and inc state_index
-            state = episode_data[state_index][0]
+            state = episode_states[state_index]
             state_index += 1
 
-            # extract x, y, and convert to 0-based indexing
-            (x, y) = (state[0]-1, state[1]-1)
+            # extract (x, y) position of the agent and convert to
+            # 0-based indexing
+            agent_position = (state[0]-1, state[1]-1)
 
-            # set next action
-            grid = generate_black_grid(num_rows, num_cols)
-            grid[y][x] = 1
+            # generate an updated representation of the grid
+            grid = generate_grid(num_rows, num_cols, agent_position)
 
-            time_elapsed_since_action_update = 0
+            # update the screen with the current contents of the grid
+            update_screen(screen, grid)
+            time_since_update = 0
 
-            # set the screen background
-            screen.fill(BLACK)
+        # test whether to exit the loop
+        for event in pygame.event.get():
+            # if the user clicked close, then exit the animation loop
+            if event.type == pygame.QUIT:
+                done = True
 
-            # draw the grid
-            for row in range(num_rows):
-                for column in range(num_cols):
-                    color = WHITE
-                    if grid[row][column] == 1:
-                        color = GREEN
-                    pygame.draw.rect(screen,
-                                     color,
-                                     [(MARGIN + WIDTH) * column + MARGIN,
-                                      (MARGIN + HEIGHT) * row + MARGIN,
-                                      WIDTH,
-                                      HEIGHT])
-        # freeze on the last state of the last episode
-        elif state_index >= episode_length:
-            time.sleep(.200)
-            state = episode_data[-1][-1]
-            # extract x, y, and convert to 0-based indexing
-            (x, y) = (state[0]-1, state[1]-1)
-
-            # set next action
-            grid = generate_black_grid(num_rows, num_cols)
-            grid[y][x] = 1
-
-            time_elapsed_since_action_update = 0
-
-            # set the screen background
-            screen.fill(BLACK)
-
-            # draw the grid
-            for row in range(num_rows):
-                for column in range(num_cols):
-                    color = WHITE
-                    if grid[row][column] == 1:
-                        color = GREEN
-                    pygame.draw.rect(screen,
-                                     color,
-                                     [(MARGIN + WIDTH) * column + MARGIN,
-                                      (MARGIN + HEIGHT) * row + MARGIN,
-                                      WIDTH,
-                                      HEIGHT])
-
-        # update screen
-        pygame.display.flip()
-
-    # when done main loop, quit the simulation
+    # when done main loop, quit pygame
     pygame.quit()
+
+# utility functions ------------------------------------------------------------
+
+# get a list of episode states from the episode data
+def get_episode_states(episode_data):
+    episode_states = []
+    num_steps = len(episode_data)
+    for i in range(num_steps):
+        # note that episode_data[i] is of form:
+        # (state, action, reward, next_state)
+        state = episode_data[i][0]
+        episode_states.append(state)
+    # the final state of the episode only appears as a 'next_state' value
+    episode_states.append(episode_data[-1][3])
+    return episode_states
+
+# re-generate the grid given its size and the agent's position
+def generate_grid(num_rows, num_cols, agent_position):
+    (x,y) = agent_position
+    grid = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
+    grid[y][x] = 1
+    return grid
+
+# update the screen with the current contents of the grid
+def update_screen(screen, grid):
+    # set the screen background
+    screen.fill(BLACK)
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    # draw each square in the grid
+    for row in range(num_rows):
+        for column in range(num_cols):
+            color = WHITE
+            # if the value at a given row and column is 1, that is the agent's
+            # location, thus set color to a non-white value
+            if grid[row][column] == 1:
+                color = GREEN
+            # define the square (rectangle) to draw
+            rectangle_left_edge = (MARGIN + WIDTH) * column + MARGIN
+            rectangle_top_edge = (MARGIN + HEIGHT) * row + MARGIN
+            rectangle = [rectangle_left_edge, rectangle_top_edge, WIDTH, HEIGHT]
+            # draw the square (rectangle)
+            pygame.draw.rect(screen, color, rectangle)
+    # update screen
+    pygame.display.flip()
