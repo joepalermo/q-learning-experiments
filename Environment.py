22e7c695b1f04f10bba0dbc740dcd28091965c41
Joseph Palermo <josephpalermo@Josephs-MacBook-Air.local>
2017-04-27 20:35:41 -0400

    re-writing Environment.py to be an environment with a moving goal that is contained in state

--- Environment.py
+++ Environment.py
@@ -8,90 +8,103 @@ class Environment:
         self.y_limit = 5
         # define the action space
         self.action_space = ["up", "left", "down", "right"]
-        # define the goal state
-        self.goal_states = [(5,5)]
-        # construct a map from states to rewards
-        self.state_to_reward = {(5,5): 1}
-        # construct a map from state-actions pairs to reward
-        self.construct_reward_map()
-
-    # construct a map from state-action pairs to reward
-    def construct_reward_map(self):
-        self.reward_map = {}
-        for state in self.state_to_reward:
-            for action in self.action_space:
-                (x, y) = state
-                if action == "up":
-                    state_action_pair = ((x,y+1), action)
-                    self.reward_map[state_action_pair] = \
-                        self.state_to_reward[state]
-                elif action == "left":
-                    state_action_pair = ((x+1,y), action)
-                    self.reward_map[state_action_pair] = \
-                        self.state_to_reward[state]
-                elif action == "down":
-                    state_action_pair = ((x,y-1), action)
-                    self.reward_map[state_action_pair] = \
-                        self.state_to_reward[state]
-                elif action == "right":
-                    state_action_pair = ((x-1,y), action)
-                    self.reward_map[state_action_pair] = \
-                        self.state_to_reward[state]
+        self.reward_map = {'found_goal': 1}
 
     # reset state in a random non-goal state
     def reset(self):
         while True:
-            x = randint(1, self.x_limit)
-            y = randint(1, self.y_limit)
-            if (x, y) not in self.goal_states:
+            agent_x = randint(1, self.x_limit)
+            agent_y = randint(1, self.y_limit)
+            goal_x = randint(1, self.x_limit)
+            goal_y = randint(1, self.y_limit)
+            if (agent_x, agent_y) != (goal_x, goal_y)
                 break
-        return (x, y)
+        return {'agent': (agent_x, agent_y), 'goal': (goal_x, goal_y)}
 
     # get the succeeding state resulting from taking a given action in a given
     # state
     def state_transition(self, state, action):
-        (x, y) = state
+        # account for the agent's movement
+        (agent_x, agent_y) = state['agent']
         if action == "up":
-            next_state = (x, y-1)
+            next_agent_state = (agent_x, agent_y-1)
         elif action == "down":
-            next_state = (x, y+1)
+            next_agent_state = (agent_x, agent_y+1)
         elif action == "left":
-            next_state = (x-1, y)
+            next_agent_state = (agent_x-1, agent_y)
         elif action == "right":
-            next_state = (x+1, y)
-        # check and correct for the possibility of state going out of bounds
-        if next_state[0] > self.x_limit:
-            return (self.x_limit, next_state[1])
-        elif next_state[1] > self.y_limit:
-            return (next_state[0], self.y_limit)
-        elif next_state[0] < 1:
-            return (1, next_state[1])
-        elif next_state[1] < 1:
-            return (next_state[0], 1)
-        # else in bounds
-        else:
-            return next_state
+            next_agent_state = (agent_x+1, agent_y)
+        # correct for the possibility of agent state going out of bounds
+        if next_agent_state[0] > self.x_limit:
+            next_agent_state = (self.x_limit, next_agent_state[1])
+        elif next_agent_state[1] > self.y_limit:
+            next_agent_state = (next_agent_state[0], self.y_limit)
+        elif next_agent_state[0] < 1:
+            next_agent_state = (1, next_agent_state[1])
+        elif next_agent_state[1] < 1:
+            next_agent_state = (next_agent_state[0], 1)
+
+        # acount for the goal's movement
+        (goal_x, goal_y) = state['goal']
+        goal_action = self.action_space[randint(0, len(self.action_space)-1)]
+        if goal_action == "up":
+            next_goal_state = (goal_x, goal_y-1)
+        elif goal_action == "down":
+            next_goal_state = (goal_x, goal_y+1)
+        elif goal_action == "left":
+            next_goal_state = (goal_x-1, goal_y)
+        elif goal_action == "right":
+            next_goal_state = (goal_x+1, goal_y)
+        # correct for the possibility of agent state going out of bounds
+        if next_goal_state[0] > self.x_limit:
+            next_goal_state = (self.x_limit, next_goal_state[1])
+        elif next_goal_state[1] > self.y_limit:
+            next_goal_state = (next_goal_state[0], self.y_limit)
+        elif next_goal_state[0] < 1:
+            next_goal_state = (1, next_goal_state[1])
+        elif next_goal_state[1] < 1:
+            next_goal_state = (next_goal_state[0], 1)
+
+        return hashabledict({'agent': next_agent_state,
+                             'goal': next_goal_state})
 
     # get step data resulting from taking a given action in a given state
     def step(self, state, action):
         next_state = self.state_transition(state, action)
-        reward = self.reward(state, action)
-        done = next_state in self.goal_states
+        reward = self.reward(next_state)
+        done = self.done(next_state)
         return (next_state, reward, done)
 
-    # get the reward that results from taking a given action in a given state
-    def reward(self, state, action):
-        if (state, action) in self.reward_map:
-            return self.reward_map[(state, action)]
+    # get the reward that results from arriving it to a particular state
+    def reward(self, state):
+        if state['agent'] == next_state['goal']
+            return self.reward_map['found_goal']
         else:
             return 0
+
+    # determine whether the episode is complete
+    def done(self, state):
+        if state['agent'] == state['goal']:
+            return True
+        else:
+            return False
 
     # initialize the Q-table to value 0 for all state-action pairs
     def initialize_q_table(self):
         q_table = {}
-        for x in range(1, self.x_limit+1):
-            for y in range(1, self.y_limit+1):
+        for agent_x in range(1, self.x_limit+1):
+            for agent_y in range(1, self.y_limit+1):
                 for action in self.action_space:
-                    state = (x, y)
-                    q_table[(state, action)] = 0
+                    for goal_x in range(1, self.x_limit+1):
+                        for goal_y in range(1, self.y_limit+1):
+                            q_table[
+                            (
+                                hashabledict(
+                                    {
+                                        'agent': (agent_x, agent_y),
+                                        'goal': (goal_x, goal_y)
+                                    }
+                                ),
+                                action
+                            )] = 0
         return q_table
